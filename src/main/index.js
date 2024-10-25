85cfe78040ddf5a5ffa40aaf1c2ba9d9a6d9dc1f
Francisco Santos <franciscosantos@moobitech.com.br>
2024-10-24 22:50:59 -0300

    feat: implementando estrutura inicial do projeto

---
+++ src/main/index.js
@@ -0,0 +1,11 @@
+import {Container} from "./styled";
+import {Text} from "../components/Text";
+
+
+export default function Main() {
+	return (
+		<Container>
+			<Text weight={600}>MiaSkok Dermocosméticos</Text>
+		</Container>
+	)
+}
