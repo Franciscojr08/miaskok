ca68beab724fa6909d7a5b866ca93e3071f28c57
DKlayver <92654447+DKlayver@users.noreply.github.com>
2024-11-03 00:51:49 -0300

    style: alterando button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -4,7 +4,15 @@ import {Container} from "./style";
 export default function Button({ children, onPress, disable, primary = true}) {
 	return (
 		<Container onPress={onPress} disable={disable} primary={primary}>
-			<Text color={primary ? "#ffffff" : "#333"}>{children}</Text>
+			<Text color="black" weight={600}>{children}</Text>
 		</Container>
 	);
 }
+
+/* versão anterior -Francisco
+
+<Container onPress={onPress} disable={disable} primary={primary}>
+			<Text color={primary ? "#ffffff" : "#333"}>{children}</Text>
+		</Container>
+		
+*/
