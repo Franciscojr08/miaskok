a3b73225bcd88ba6a72e90541c7985d6ed5b8922
Francisco Santos <franciscosantos@moobitech.com.br>
2024-11-13 18:43:24 -0300

    fix: removendo importações desnecessárias

--- src/main/index.js
+++ src/main/index.js
@@ -1,7 +1,5 @@
 import {Container,} from "./styled.js";
-import TelaInicio from "../components/TelaInicio";
 import Toast from "react-native-toast-message";
-import Header from "../components/Header";
 import Navigator from "../components/Navigator";
 
 export default function Main() {
