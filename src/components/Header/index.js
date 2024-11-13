48a63ea9903ac2d3eabd223d6f7ee7b9f564f647
DKlayver <92654447+DKlayver@users.noreply.github.com>
2024-11-13 02:49:04 -0300

    Adicionando_Botao_Voltar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,36 @@
-import { TouchableOpacity } from 'react-native'
+import { Alert, TouchableOpacity } from 'react-native'
 import logo from '../../assets/images/logo.png'
-import { Container, ImageContainer } from './styles'
+import voltar from '../../assets/images/voltar.png'
+import { Container, ImageContainer, BackContainer, ContainerPrincipal, ContainerSecundario } from './styles'
 import {useNavigation} from "@react-navigation/native";
+import { Text } from '../Text';
+import { useRoute } from '@react-navigation/native';
 
 export default function Header() {
     const navigation = useNavigation();
+    const route = useRoute();
 
+
+    if(route.name === 'TelaInicio'){
+        return(
+            <ContainerPrincipal>
+                {console.log('aaaaaaaa')}
+                <TouchableOpacity onPress={() => navigation.navigate('TelaInicio')}>
+                    <ImageContainer source={logo} />
+                </TouchableOpacity>
+            </ContainerPrincipal>
+        )
+    }else{
     return (
-        <Container>
+        <ContainerSecundario>
+            {console.log('bbbbb')}
+            <TouchableOpacity onPress={() => navigation.goBack()}>
+                <BackContainer source={voltar}/>
+            </TouchableOpacity>
             <TouchableOpacity onPress={() => navigation.navigate('TelaInicio')}>
                 <ImageContainer source={logo} />
-            </TouchableOpacity>
-        </Container>
+            </TouchableOpacity>  
+        </ContainerSecundario>
     )
+    }
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,15 +1,35 @@
 import styled from "styled-components/native";
 
-export const Container = styled.View`
+export const ContainerPrincipal = styled.View`
     align-items: center;
     justify-content: center;
 	max-height: 90px;
 	background-color: #fff;
 `;
 
+export const ContainerSecundario = styled.View`
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-around;
+	max-height: 90px;
+	background-color: #fff;
+`;
+export const constContainerBack = styled.View`
+
+`
+
 export const ImageContainer = styled.Image`
-    width: 200px;
-    height: 200px;
+    max-width: 200px;
+    max-height: 200px;
     resize-mode: contain;
-    margin-right: 5px; 
+`;
+
+
+export const BackContainer = styled.Image`
+    justify-content:center;
+    max-width: 40px;
+    max-height: 50px;
+    resize-mode: contain;
+    margin-right: 50px; 
+    margin-Bottom: 15px;
 `;
